06ff1ccebf216b2500d1fde7dc7a6a614153e500
Muhammad Umair <muhammad.umair1019@gmail.com>
2026-08-29 21:56:09 +0500

    switch, while and do while loop in js

---
+++ module-02/javascript/lesson-16/app.js
@@ -0,0 +1,142 @@
+
+// for (let i = 0; i < 2; i++) {
+//     console.log(i)
+// }
+
+// console.log(i)
+
+// let i = 0;
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 0)
+
+
+
+// let i = 0;
+
+// while (i < 1) {
+//     console.log(i)
+
+//     i++;
+// }
+
+
+// for (let i = 0; i < 0; i++) {
+//     console.log(i)
+// }
+
+
+
+
+
+// ------------------------------------------------
+// let day = 7;
+
+// switch (day) {
+//     case 0:
+//         console.log("Sunday");
+//         break;
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+
+//     default:
+//         console.log("Shoot me now!")
+
+// }
+
+// if (day === 0) {
+//     console.log("Sunday")
+// } else if (day === 1) {
+//     console.log("Monday")
+// } else if (day === 2) {
+//     console.log("Tuesday")
+// } else if (day === 3) {
+//     console.log("Wednesday")
+// } else if (day === 4) {
+//     console.log("Thursday")
+// } else if (day === 5) {
+//     console.log("Friday")
+// } else if (day === 6) {
+//     console.log("Saturday")
+// } else {
+//     console.log("Shoot me now!")
+// }
+
+
+
+// let day = 2;
+
+// if (day === 0) {
+//     console.log("Sunday")
+// } else if (day === 1) {
+//     console.log("Monday")
+// } else if (day === 2) {
+//     console.log("Tuesday")
+// } else if (day === 3) {
+//     console.log("Wednesday")
+// } else if (day === 4) {
+//     console.log("Thursday")
+// } else if (day === 5) {
+//     console.log("Friday")
+// } else if (day === 6) {
+//     console.log("Saturday")
+// }
+
+
+
+
+
+
+// -----------------------------------------------
+// call by ref
+// function fn(arr) {
+//     arr[0] = 100;
+// }
+
+// let arr = [10, 20, 30];
+// fn(arr)
+
+// console.log(arr)
+
+// call by value
+// function fn1(x) {
+//     x = x + 10;
+// }
+
+// let x = 10;
+// fn1(x)
+
+// console.log(x)
+
+
+
+
+// var k = 10;
+// function x() {
+//     var z = 20;
+//     k = 30;
+//     console.log(k)
+//     console.log(z)
+
+// }
+
+// console.log(k)
+// x()
