7eba5f88f2ab28106ec25226e4d3e9c21ed53f64
Muhammad Umair <muhammad.umair1019@gmail.com>
2026-09-18 20:23:42 +0500

    todo app

--- module-02/javascript/lesson-20/app.js
+++ module-02/javascript/lesson-20/app.js
@@ -1,34 +1,70 @@
 const ul = document.querySelector("ul");
-const addTodo = document.querySelector("button");
-
-// const list = ul.children;
+const addTodo = document.querySelector("#add");
+const addInput = document.querySelector("#addInput")
 
 ul.addEventListener("click", function (e) {
+
+    if (e.target.classList.contains('del')) {
+        e.target.parentElement.remove()
+    }
+
     if (e.target.nodeName === 'LI') {
-        e.target.style.textDecoration = "line-through"
+        // e.target.style.textDecoration = "line-through"
+        e.target.classList.toggle('list-item')
     }
 })
 
-// for (let i = 0; i < list.length; i++) {
-//     list[i].addEventListener("click", function () {
-//         list[i].style.textDecoration = "line-through"
-//     })
-// }
 
+console.log(addInput)
 addTodo.addEventListener("click", function () {
-    console.log("Click")
 
+    let div = document.createElement("div");
+    let button = document.createElement("button");
     let li = document.createElement("li");
 
-    li.textContent = 'Item 4';
+    li.textContent = addInput.value;
+    button.textContent = 'Delete';
+    button.setAttribute("class", "del");
 
+    div.appendChild(li);
+    div.appendChild(button);
 
-    // ul.innerHTML = ul.innerHTML + `<li> Item 4 </li>`;
+    div.classList.add("item")
 
-    ul.appendChild(li)
-
-
+    ul.appendChild(div)
+    addInput.value = ''
 })
+
+
+// addTodo.addEventListener("click", function () {
+//     console.log("Click")
+//     //     <div class="item">
+//     //     <li>Item 1</li>
+//     //     <button id="del">Delete</button>
+//     // </div>
+
+//     let div = document.createElement("div");
+//     let button = document.createElement("button");
+//     let li = document.createElement("li");
+
+//     li.textContent = 'Item 4';
+//     button.textContent = 'Delete';
+//     button.setAttribute("class", "del");
+
+//     div.appendChild(li);
+//     div.appendChild(button);
+
+//     div.classList.add("item")
+
+//     console.log(div)
+
+
+//     // ul.innerHTML = ul.innerHTML + `<li> Item 4 </li>`;
+
+//     ul.appendChild(div)
+
+
+// })
 
 
 
